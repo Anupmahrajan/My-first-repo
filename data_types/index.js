993865f99507c41e77d510422b9990e89a56e651
GHOUL_ANUP <mhranup@gmail.com>
2020-09-28 08:46:41 +0545

    data type file made

---
+++ data_types/index.js
@@ -0,0 +1,8 @@
+let variable_name_second ,another_variable = 'hello class';
+let float_value = 1.21 ;
+console.log(variable_name_second);
+console.log(another_variable);
+console.log(float_value);
+console.log("-can i type anything>???");
+console.log(typeof variable_name_second);
+console.log(typeof float_value);
