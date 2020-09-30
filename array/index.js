8c17d86a942927542b486624bf2e79510965d7b3
GHOUL_ANUP <mhranup@gmail.com>
2020-09-30 08:55:26 +0545

    array folder added

---
+++ array/index.js
@@ -0,0 +1,8 @@
+let fruit =['apple','mango','pears'];
+console.log(fruit);
+let name = new Array('MA','TIMI','HAMI');
+console.log(name);
+
+let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+console.log(newArr[2][2]);
+console.log (newArr.length);
