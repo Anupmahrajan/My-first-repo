e75108323716fd94266bd319044613668a6c6ca6
GHOUL_ANUP <mhranup@gmail.com>
2020-10-04 09:05:48 +0545

    added new task

--- array/index.js
+++ array/index.js
@@ -1,8 +1,34 @@
-let fruit =['apple','mango','pears'];
-console.log(fruit);
-let name = new Array('MA','TIMI','HAMI');
-console.log(name);
+// let fruit =['apple','mango','pears'];
+// console.log(fruit);
+// let name = new Array('MA','TIMI','HAMI');
+// console.log(name);
 
-let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
-console.log(newArr[2][2]);
-console.log (newArr.length);
+// let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+// console.log(newArr[2][2]);
+// console.log (newArr.length);
+// newArr.unshift('This is not array');
+// console.log (newArr);
+// fruit.splice(1,3,'lu kha');
+// console.log (fruit);
+ // let dc = ['superman','batman',['flash','wonderwoman'],'aquaman'];
+// dc[2].unshift('joker');
+// console.log(dc)
+// dc.splice(4,0,'joker');
+// console.log(dc)
+// delete dc [2];
+// console.log(dc);
+// dc.splice(0,dc.length);
+// console.log(dc);
+// let name = ['anup','shyam'];
+// let cast = [name.slice(1)];
+// console.log(name);
+// console.log(cast);
+// let dc = ['superman','batman','aquaman','flash','wonderwoman'];
+// let marverl = [dc.slice(3)];
+// console.log (dc);
+// console.log (marverl);
+let me = {};
+me.name='anup';
+me.cast='maharjan';
+me.age=21;
+console.log(me);
